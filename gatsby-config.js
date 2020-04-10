aba2110b690dc2db078a196ee0267b1a3ecdb927
melakakini <mymelakakini@gmail.com>
2020-04-10 21:45:59 +0800

    Update gatsby-config.js
    customized the data to reflect Best Melaka Guide

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,12 +7,12 @@
 module.exports = {
   /* Your site config here */
   siteMetadata: {
-    title: `Delog`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://delog-w3layouts.netlify.com/`,
+    title: `Best Melaka Guide`,
+    description: `The Best Guide on Melaka powered by Gatsby.`,
+    siteUrl: `https://blissful-spence-c10b11.netlify.com`,
     home: {
-      title: `Hi! I'm Delog`,
-      description: `I have been specifically designed to become a digital home for designers and developers, help them build amazing professional looking websites with ease. You don't have to worry about nitty gritty of web hosting services to run a blog and yet take full advantage of CMS to manage content :)`,
+      title: `Best Melaka Guide`,
+      description: `Presenting the Best of Melaka in one website and updated for your convenience.`,
     },
     /* W3Layouts domain verification key for contact forms https://my.w3layouts.com/Forms/ */
     w3l_dom_key: `5e609f7a2d23fCF_Domain_verify` 
